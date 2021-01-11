4fd2cd5251f9a4c9cbb3f0d4dc630599f35edfe8
KarlaRosas19 <49324882+KarlaRosas19@users.noreply.github.com>
2021-01-11 11:21:35 +0100

    Fiche et Tableau

--- src/main/java/jpa/Fiche.java
+++ src/main/java/jpa/Fiche.java
@@ -1,25 +1,48 @@
 package jpa;
+import com.sun.javafx.binding.StringFormatter;
 import javafx.scene.control.Tab;
 
+import java.sql.Time;
 import java.util.Date;
 import java.util.List;
 
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.ManyToMany;
-import javax.persistence.OneToMany;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 import org.hibernate.annotations.ManyToAny;
 
 @Entity
 public class Fiche {
+
+
+    private long id;
+
+    private String libelle;
+    private Tableau tableau;
+    private Date dateButoire;
+    private Utilisateur utilisateur;
+    private Time besoinMinutes;
+    private List<Tags> tags;
+    private String lieu;
+    private  String url;
+
+
+
+    public Fiche(){
+     //  super();
+   }
+
+   public Fiche(String libelle){
+       this.libelle = libelle;
+   }
+
     @Id
     @GeneratedValue
-    long id;
+    public long getId() {
+        return id;
+    }
 
-    String libelle;
+    public void setId(long id) {
+        this.id = id;
+    }
 
     public String getLibelle() {
         return libelle;
@@ -28,6 +51,64 @@ public class Fiche {
     public void setLibelle(String libelle) {
         this.libelle = libelle;
     }
+
+    public Date getDateButoire() {
+        return dateButoire;
+    }
+
+    public void setDateButoire(Date dateButoire) {
+        this.dateButoire = dateButoire;
+    }
+
+    public Utilisateur getUtilisateur() {
+        return utilisateur;
+    }
+
+    public void setUtilisateur(Utilisateur utilisateur) {
+        this.utilisateur = utilisateur;
+    }
+
+    public Time getBesoinMinutes() {
+        return besoinMinutes;
+    }
+
+    public void setBesoinMinutes(Time besoinMinutes) {
+        this.besoinMinutes = besoinMinutes;
+    }
+
+    public List<Tags> getTags() {
+        return tags;
+    }
+
+    public void setTags(List<Tags> tags) {
+        this.tags = tags;
+    }
+
+    public String getLieu() {
+        return lieu;
+    }
+
+    public void setLieu(String lieu) {
+        this.lieu = lieu;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    @ManyToOne
+    public Tableau getTableau() {
+        return tableau;
+    }
+
+    public void setTableau(Tableau tableau) {
+        this.tableau = tableau;
+    }
+
 }
 
 
